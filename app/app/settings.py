4819d2cfe0c71dd1d7211e8e3392cb79482a3c93
Priyanka Pakki <priyanka.pakki@gmail.com>
2023-04-21 21:13:36 +0200

    permission error

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,15 +21,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-jq-fnz7t4=#(yv+yqp!jzw$$09k2s4vfhm(i(k)ab*o@=p!))p"  # noqa
-)
+# SECRET_KEY = (
+#     "django-insecure-jq-fnz7t4=#(yv+yqp!jzw$$09k2s4vfhm(i(k)ab*o@=p!))p"  # noqa
+# )
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get("ALLOWED_HOSTS", "").split(","),
+    )
+)
 # Application definition
 
 INSTALLED_APPS = [
